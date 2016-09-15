7f1572b0eeb748ef1faa19f07a7c9c17751b37a6
matt <mattcllhn@gmail.com>
2016-09-15 14:28:57 -0500

    Finished DB creation statements

--- database.sql
+++ database.sql
@@ -3,3 +3,11 @@ CREATE TABLE employee (
 	first_name VARCHAR(20),
 	last_name VARCHAR(20)
 );
+
+CREATE TABLE floor (
+	id SERIAL PRIMARY KEY,
+	table_name VARCHAR(10),
+	capacity INT,
+	server_id INT REFERENCES employee(id) ON DELETE SET NULL,
+	status VARCHAR(10)
+);
